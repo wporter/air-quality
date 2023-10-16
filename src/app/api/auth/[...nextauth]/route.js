26a48a762c1816096f9e3a9d7331990dce1588fb
rsabe004 <rsabe004@ucr.edu>
2023-10-16 14:38:12 -0700

    adding firebase auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,14 @@
 import NextAuth from "next-auth";
 import googleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firestore = (
+  firebase.apps[0] ?? firebase.initializeApp(/* your config */)
+).firestore();
+
 export const options = {
   providers: [
     // eslint-disable-next-line new-cap
@@ -8,6 +17,7 @@ export const options = {
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
   ],
+
   // pages: {
   //     signIn: "/signIn"
   // }
