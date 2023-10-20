578a1428642af455e8ca4491c5723626113e395b
Divyank Shah <divyank.shah.2016@gmail.com>
2023-10-20 15:26:37 -0700

    add project id

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ export const options = {
   // eslint-disable-next-line new-cap
   adapter: FirestoreAdapter({
     credential: cert({
-      projectId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
     }),
