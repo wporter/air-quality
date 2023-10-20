5350e35488e6271635c261950301133a80e196ac
Paulian Le <paulianle7@gmail.com>
2023-10-20 15:53:03 -0700

    Edit nextauth pathway

--- src/app/utils/authenticate.js
+++ src/app/utils/authenticate.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "./auth/[...nextauth]";
+import { authOptions } from "../api/auth/[...nextauth]";
 
 export const authenticate = async (restrictions = {}) => {
   const session = await getServerSession(authOptions);
