60d20d9e011da13d252dd96109916ad08e6e2b99
Paulian Le <paulianle7@gmail.com>
2023-11-28 16:44:21 -0800

    Removed global.css extra stuff & added tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        air: {
+          black: "#1D1D1D",
+          green: "#165C21",
+        },
+      },
     },
   },
   plugins: [],
