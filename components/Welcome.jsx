3c418cf056d835307e589e4bd5f54f2820776b69
Paulian Le <paulianle7@gmail.com>
2023-11-28 17:30:01 -0800

    Added placeholder nav bar and created welcome component

---
+++ components/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+	return (
+		<div className='flex flex-row items-center justify-between mx-80 h-screen'>
+			<p>i am the landing page :D</p>
+		</div>
+	);
+};
+
+export default Welcome;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
 "use client";
 import { signIn } from "next-auth/react";
+import Welcome from "components/Welcome.jsx";
 
 const Page = () => {
   return (
-    <div>
-      <button onClick={() => signIn("google")}>Sign in</button>
+    <div className="m-0 p-0 h-screen overflow-hidden">
+      {/* Project title */}
+      <title>Omega Initiative</title>
+
+      {/* Nav Bar Component - placeholder after sharan finishes */}
+      <div className="flex justify-between p-5 bg-gray-500">
+        <p className="text-air-green">Air Nav Bar!</p>
+        <button onClick={() => signIn("google")}>Sign in</button>
+      </div>
+
+      {/* welcome page component */}
+      <div>
+        <Welcome />
+      </div>
     </div>
   );
 };
