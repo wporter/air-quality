57deb21713a18d9580f56bcbd61af5bf7d8ac9e5
Zachary Ruiz <zacharyruiz1@gmail.com>
2023-12-07 13:14:47 -0800

    modified protectedPage to include option for component to be rendered when not authorized

--- src/components/protectedPage.jsx
+++ src/components/protectedPage.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { signIn, useSession } from "next-auth/react";
 
-const ProtectedPage = ({ fallback, children }) => {
+const ProtectedPage = ({ fallback, unauthenticatedComponent, children }) => {
   const { status } = useSession();
   const [confirmed, setConfirmed] = useState(false);
 
@@ -15,18 +15,22 @@ const ProtectedPage = ({ fallback, children }) => {
   if (status === "loading") {
     return fallback ? fallback : <></>;
   } else if (status === "unauthenticated") {
-    return (
-      <>
-        <p>{"You are not authorized to view this resource. "}</p>
-        <button
-          onClick={() => {
-            void signIn("google");
-          }}
-        >
-          Sign In
-        </button>
-      </>
-    );
+    if (unauthenticatedComponent) {
+      return <>{unauthenticatedComponent}</>;
+    } else {
+      return (
+        <>
+          <p>{"You are not authorized to view this resource. "}</p>
+          <button
+            onClick={() => {
+              void signIn("google");
+            }}
+          >
+            Sign In
+          </button>
+        </>
+      );
+    }
   }
 
   if (confirmed) {
