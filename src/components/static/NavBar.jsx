583fb92cd63c5d973a6be5ad6d1a86e0fd59aacd
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2023-12-07 15:28:02 -0800

    finished nav bar

--- src/components/static/NavBar.jsx
+++ src/components/static/NavBar.jsx
@@ -3,32 +3,13 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Link from "next/link";
 
-// const buttonStyles = {
-//   padding: "8px 34px",
-//   backgroundColor: "transparent",
-//   color: "#006400",
-//   border: "2px solid #006400",
-//   borderRadius: "4px",
-//   cursor: "pointer",
-//   fontSize: "1rem",
-//   fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
-//   fontWeight: "bold",
-// };
-
-// const landingButton = {
-//   backgroundColor: "transparent",
-//   color: "#006400",
-//   cursor: "pointer",
-//   fontSize: "1rem",
-//   fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
-//   fontWeight: "bold",
-// };
-
-// const logoTextStyles = {
-// 	fontWeight: "bold",
-// 	textTransform: "uppercase",
-// 	fontSize: "1.5rem",
-//   };
+const navStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "15px",
+  backgroundColor: "#ffffff",
+};
 
 const Navigation = () => {
   return (
@@ -37,15 +18,20 @@ const Navigation = () => {
       className="py-0 font-lexend w-full px-3 m-0 min-h-[8vh] bg-white border-acm-blue border-b-4 flex justify-between items-center"
       expand="md"
       fixed="top"
+      style={navStyles}
     >
       <Navbar.Brand className="p-0">
-        <Link
-          eventkey="1"
-          className=" px-2 no-underline ml-auto text-lg font-bold whitespace-nowrap bg-transparent text-green-900 "
-          href="/"
-        >
-          <span>OMEGA INITIATIVE</span>
-          <span className="px-2 block text-sm text-greem-500">
+        <Link eventkey="1" href="/">
+          <span className=" mb-2 px-2 no-underline ml-auto text-lg font-bold whitespace-nowrap bg-transparent text-green-900 ">
+            {" "}
+            OMEGA INITIATIVE
+          </span>
+          <span
+            className="px-2 block text-sm text-greem-500 italic text-xs m-0"
+            style={{
+              fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
+            }}
+          >
             Air quality data, Ontario
           </span>{" "}
         </Link>
