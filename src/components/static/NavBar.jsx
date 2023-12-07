5427bb2b1ba01aa59f1090b329cbf6a259e7c48b
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2023-12-07 12:37:05 -0800

    nav

--- src/components/static/NavBar.jsx
+++ src/components/static/NavBar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import Link from "next/link";
 
 const NavBar = () => {
   // Styles for the navigation bar
