af4934bb4e79a985d5939450225e4e7ea674bd1a
Paulian Le <paulianle7@gmail.com>
2023-12-28 23:30:49 -0800

    Fixed roundness

--- src/components/researcher/dashboard/Status.jsx
+++ src/components/researcher/dashboard/Status.jsx
@@ -1,6 +1,6 @@
 const Status = ({ bg, value, text, Icon, color }) => {
   return (
-    <div className={`${bg} h-1/3 p-6 rounded-md flex flex-col`}>
+    <div className={`${bg} h-1/3 p-6 rounded-lg flex flex-col`}>
       <Icon className={`${color} w-7 h-7 mb-4`} />
       <p className="font-semibold font-poppins text-xl">{value}</p>
       <p className="font-normal font-poppins text-[#6A707E] text-lg">{text}</p>
