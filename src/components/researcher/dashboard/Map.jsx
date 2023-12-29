f95ba9090620a16b3772bc1b9233a1cfc0b6648a
Paulian Le <paulianle7@gmail.com>
2023-12-28 23:23:29 -0800

    Fixed styling of status comp

--- src/components/researcher/dashboard/Map.jsx
+++ src/components/researcher/dashboard/Map.jsx
@@ -14,20 +14,21 @@ const Map = () => {
       Welcome William
       <div className="w-full h-full bg-green-500 flex">
         <Leaflet height="h-5/6" width="w-10/12" markers={[]} />
-        <div className="w-2/12 p-3">
+        <div className="w-2/12 p-3 flex flex-col space-y-16">
           <Status
             bg="bg-status-green"
             Icon={MdOutlineSignalWifi4Bar}
             color="text-green-400"
-            value={200}
-            text="sensors online"
+            value={300}
+            text="Sensors Online"
           />
+
           <Status
             bg="bg-status-red"
             Icon={MdOutlineNetworkWifi1Bar}
             color="text-red-400"
-            value={100}
-            text="sensors offline"
+            value={200}
+            text="Sensors Offline"
           />
         </div>
       </div>
