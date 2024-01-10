3c4f797496e1d5c55b8fc91cecc01773e93cefe3
Divyank Shah <divyank.shah.2016@gmail.com>
2024-01-09 16:05:04 -0800

    update colors

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,17 +7,17 @@ const Navigation = () => {
   return (
     <div className="fixed top-0 left-0 w-full bg-white flex justify-between items-center py-3 px-8 z-10 drop-shadow-xl">
       <div>
-        <p className="text-air-blue text-3xl">
+        <p className="text-air-blue-200 text-3xl">
           <span>&Omega;</span>
           <span className="font-bold"> OMEGA</span>INITIATIVE
         </p>
       </div>
 
-      <div className=" px-5 py-0.4 text-air-blue2 text-xl ml-auto">
+      <div className=" px-5 py-0.4 text-air-blue-100 text-xl ml-auto">
         <button className="px-24">ABOUT US</button>
       </div>
 
-      <div className="rounded px-5 py-0.4 text-air-blue2 border-2 border-air-blue2 text-xl">
+      <div className="rounded px-5 py-0.4 text-air-blue-100 border-2 border-air-blue-100 text-xl">
         <button onClick={() => (session ? signOut() : signIn("google"))}>
           {session ? "SIGN OUT" : "SIGN IN"}
         </button>
--- src/components/researcher/dashboard/Data.jsx
+++ src/components/researcher/dashboard/Data.jsx
@@ -72,7 +72,7 @@ const Data = () => {
   return (
     <div className="h-1/2 w-full bg-yellow-500 overflow-scroll p-2">
       <Search filters={filters} setFilters={setFilters} />
-      <div className="bg-air-blue text-white">
+      <div className="bg-air-blue-200 text-white">
         {getHeaderGroups().map(({ headers, id }) => (
           <div key={id} className="flex">
             {headers.map(({ id, column }) => (
