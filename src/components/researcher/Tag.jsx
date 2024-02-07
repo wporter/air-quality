3754abd1b4d7dc89cb036cc3f62e37cf5a91538c
Paulian Le <paulianle7@gmail.com>
2024-02-06 20:10:04 -0800

    Revert "Add Tag Tests"

--- src/components/researcher/Tag.jsx
+++ src/components/researcher/Tag.jsx
@@ -4,7 +4,6 @@ export const Tag = ({ text, color }) => {
   return (
     <div
       className={`${tags[color]} w-fit px-4 py-1 rounded flex justify-center items-center`}
-      data-cy={`${text}-tag`}
     >
       {text}
     </div>
