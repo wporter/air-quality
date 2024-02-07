3c514a4df98425c06894bb19b612e5f9141ffc52
rsabe004 <rsabe004@ucr.edu>
2024-02-06 19:42:05 -0800

    added waves svg to about page

--- src/components/about/Profile.jsx
+++ src/components/about/Profile.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 const Profile = ({ name, position, image }) => {
   return (
     <div className="text-center">
-      <Image
+      <img
         className="rounded-full"
         src="https://m.media-amazon.com/images/I/810XSuEz1vL.jpg"
         alt="User Profile Pictures"
