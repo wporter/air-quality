ba0c446b3fb5e784880178bf79c87b830616c0a4
shahdivyank <divyank.shah.2016@gmail.com>
2024-02-06 19:46:40 -0800

    cleanup

--- src/components/researcher/dashboard/Dropdown.jsx
+++ src/components/researcher/dashboard/Dropdown.jsx
@@ -11,21 +11,22 @@ const Dropdown = ({ options, option, setOption }) => {
         className="flex items-center justify-between px-2 hover:cursor-pointer text-gray-400"
       >
         {option.text}
-        <GoTriangleDown className={`${toggle && "rotate-180"} text-2xl`} />
+        <GoTriangleDown
+          className={`${toggle && "rotate-180"} duration-300 text-2xl`}
+        />
       </div>
 
       <div
-        className={`absolute bg-white w-full ${
-          toggle ? "border-gray-400 border-2" : ""
-        } my-2 text-nav-black rounded-lg`}
+        className={`absolute left-0 bg-white w-full my-2 text-nav-black rounded-lg ${
+          toggle && "border-gray-400 border-2"
+        }`}
       >
         {toggle &&
           options.map(({ accessor, text }, index) => (
             <div
-              className={`hover:cursor-pointer px-4 py-1 hover:bg-nav-hover transition-colors duration-300 ${
+              className={`hover:cursor-pointer px-4 py-1 hover:bg-nav-hover transition-colors duration-300 first:rounded-t-lg last:rounded-b-lg ${
                 index < 2 && "border-b-2 border-dropdown-grey"
-              } ${"first:rounded-t-lg"}
-              ${"last:rounded-b-lg"}`}
+              }`}
               key={index}
               onClick={() => {
                 setOption({ accessor, text });
