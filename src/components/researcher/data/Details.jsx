0ce06895f07fdd6c39c6f73bd3e32a0db02648f9
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2024-02-06 16:29:51 -0800

    rounded the table more

--- src/components/researcher/data/Details.jsx
+++ src/components/researcher/data/Details.jsx
@@ -2,7 +2,7 @@ import { MdRefresh } from "react-icons/md";
 
 const Details = ({ fields, refresh }) => {
   return (
-    <div className="w-fit border border-gray-300 font-neue rounded pl-9 pr-9">
+    <div className="w-fit border border-gray-300 font-neue rounded-lg pl-9 pr-9">
       <div className="flex justify-between items-center border-b-[2px]  border-gray-300 mx-[-1.5rem] px-6 py-2">
         <p className="text-lg font-bold px-2 ">DEVICE DETAILS</p>
         <MdRefresh onClick={() => refresh()} />
