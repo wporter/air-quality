dbd68e135a2932eb5f0128d63c25981c55e3c604
shahdivyank <divyank.shah.2016@gmail.com>
2024-02-06 20:22:16 -0800

    custom error component tests

--- cypress/component/Error.cy.js
+++ cypress/component/Error.cy.js
@@ -7,10 +7,21 @@ describe("Tag", () => {
     cy.get('[data-cy="error-status"]').contains("404");
     cy.get('[data-cy="error-name"]').contains("Page Not Found.");
   });
+
   it("500 Internal Server Error", () => {
     cy.mount(<Error status="500" name="Internal Server Error" />);
 
     cy.get('[data-cy="error-status"]').contains("500");
     cy.get('[data-cy="error-name"]').contains("Internal Server Error");
+  });
+
+  it("000 Custom Error", () => {
+    cy.mount(
+      <Error status="000" name="Custom Error" message="Custom Message" />,
+    );
+
+    cy.get('[data-cy="error-status"]').contains("000");
+    cy.get('[data-cy="error-name"]').contains("Custom Error");
+    cy.get('[data-cy="error-message"]').contains("Custom Message");
   });
 });
