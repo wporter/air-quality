b9923893c7b8572b61b3543028d798bc95846f55
shahdivyank <divyank.shah.2016@gmail.com>
2024-02-20 12:48:50 -0800

    remove next auth

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,8 @@
 "use client";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { ImOmega } from "react-icons/im";
 
 const Navigation = () => {
-  const { data: session } = useSession();
-
   return (
     <div className="w-full bg-white flex justify-between items-center py-3 px-8 z-10 drop-shadow-xl">
       <Link href="/" className="flex items-center">
@@ -25,9 +22,7 @@ const Navigation = () => {
       </div>
 
       <div className="rounded-lg px-6 py-1 text-air-blue-200 border-2 border-air-blue-200 text-xl font-medium">
-        <button onClick={() => (session ? signOut() : signIn("google"))}>
-          {session ? "SIGN OUT" : "SIGN IN"}
-        </button>
+        <button>Sign In</button>
       </div>
     </div>
   );
