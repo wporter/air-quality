4ef545c5995b96d08c747c1263baf91524db3073
rsabe004 <rsabe004@ucr.edu>
2024-02-24 22:03:28 -0800

    adjusted styling and spacing

--- components/about/Profile.jsx
+++ components/about/Profile.jsx
@@ -1,13 +1,31 @@
 const Profile = ({ name, position, image }) => {
   return (
-    <div className="text-center">
-      <img
-        className="rounded-full"
-        src="https://m.media-amazon.com/images/I/810XSuEz1vL.jpg"
-        alt="User Profile Pictures"
-      />
-      <p className="font-semibold text-xl">{name}</p>
-      <p>{position}</p>
+    <div
+      style={{
+        display: "inline-flex",
+        gap: "10px",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+      }}
+    >
+      <div
+        style={{
+          display: "inline-flex",
+          flexDirection: "column",
+          gap: "10px",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <img
+          className="rounded-full"
+          src="https://m.media-amazon.com/images/I/810XSuEz1vL.jpg"
+          alt="User Profile Pictures"
+          style={{ width: "250px", height: "250px" }}
+        />
+        <p className="font-semibold text-xl">{name}</p>
+        <p>{position}</p>
+      </div>
     </div>
   );
 };
