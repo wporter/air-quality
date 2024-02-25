c192e11624f6740b0bd5f77a452e10dff7538b96
rsabe004 <rsabe004@ucr.edu>
2024-02-24 23:03:18 -0800

    tailwind styling

--- components/about/Profile.jsx
+++ components/about/Profile.jsx
@@ -1,27 +1,11 @@
 const Profile = ({ name, position, image }) => {
   return (
-    <div
-      style={{
-        display: "inline-flex",
-        gap: "10px",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-      }}
-    >
-      <div
-        style={{
-          display: "inline-flex",
-          flexDirection: "column",
-          gap: "10px",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
+    <div className="justify-start items-start inline-flex">
+      <div className="inline-flex flex-col justify-start gap-3 flex-start items-center ">
         <img
-          className="rounded-full"
+          className="rounded-full w-[250px] h-[250px]"
           src="https://m.media-amazon.com/images/I/810XSuEz1vL.jpg"
           alt="User Profile Pictures"
-          style={{ width: "250px", height: "250px" }}
         />
         <p className="font-semibold text-xl">{name}</p>
         <p>{position}</p>
