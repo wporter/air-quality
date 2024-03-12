e274ce1ec9417c200f44027ac6b59afd9033d612
rsabe004 <rsabe004@ucr.edu>
2024-03-11 22:06:11 -0700

    favicon added

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,29 @@ import Welcome from "@/components/Welcome";
 import Map from "@/components/Map/Map";
 import Footer from "@/components/Footer";
 import { getMarkers } from "@/utils/api";
+import Head from "next/head";
 
 const Page = async () => {
   const { items } = await getMarkers();
 
   return (
-    <div className="flex flex-col justify-center items-center relative">
-      <Welcome />
-      <div className="my-10 w-11/12">
-        <p className="my-4 font-bold text-5xl">Public View</p>
-        <Map height="h-[60vh]" width="w-full" markers={items} />
+    <>
+      <div>
+        <Head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+        </Head>
       </div>
 
-      <Footer />
-    </div>
+      <div className="flex flex-col justify-center items-center relative">
+        <Welcome />
+        <div className="my-10 w-11/12">
+          <p className="my-4 font-bold text-5xl">Public View</p>
+          <Map height="h-[60vh]" width="w-full" markers={items} />
+        </div>
+
+        <Footer />
+      </div>
+    </>
   );
 };
 
