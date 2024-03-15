166bdc79f2b27d38795bc1fd7fa276f5ddcc7520
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2024-03-14 18:18:47 -0700

    cleaned up Nav bar ui

--- src/components/researcher/SideNavigation.jsx
+++ src/components/researcher/SideNavigation.jsx
@@ -5,22 +5,27 @@ import { FiHelpCircle } from "react-icons/fi";
 
 const SideNavigation = () => {
   return (
-    <div className="w-[12%] h-screen flex flex-col justify-between">
+    <div className="relative z-50 w-48 h-screen flex flex-col justify-between shadow-2xl">
       <div className="flex flex-col justify-center items-center space-y-2 mt-8">
-        <ImOmega className="text-air-blue-200 text-3xl" />
-        <p className="text-air-blue-200 text-xl font-bold">OMEGA</p>
+        <Link
+          href="/"
+          className="text-air-blue-200 flex flex-col items-center no-underline py-2 px-4"
+        >
+          <ImOmega className="text-3xl" />
+          <p className="text-xl font-bold pt-2 pb-5">OMEGA</p>
+        </Link>
         <Link
           href="/researcher"
           className="text-[#363636] flex items-center w-full no-underline py-2 px-4 font-normal hover:bg-nav-hover transition-colors duration-300"
         >
-          <MdOutlineDashboard className="text-[#898989] text-xl mx-2" />
+          <MdOutlineDashboard className="text-[#898989] text-xl mx-2 w-6 h-6" />
           <div>Dashboard</div>
         </Link>
         <Link
           href="/help"
           className="text-nav-black flex items-center w-full no-underline py-2 px-4 font-normal hover:bg-nav-hover transition-colors duration-300"
         >
-          <FiHelpCircle className="text-nav-grey text-xl mx-2" />
+          <FiHelpCircle className="text-nav-grey text-xl mx-2 w-6 h-6" />
           <div>Help</div>
         </Link>
       </div>
