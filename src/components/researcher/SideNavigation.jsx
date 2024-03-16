5d11f4c956fded2c3aed50bf1fedd16197d6bb6a
sharansingh1 <102877489+sharansingh1@users.noreply.github.com>
2024-03-15 18:06:29 -0700

    minor fixes

--- src/components/researcher/SideNavigation.jsx
+++ src/components/researcher/SideNavigation.jsx
@@ -16,16 +16,16 @@ const SideNavigation = () => {
         </Link>
         <Link
           href="/researcher"
-          className="text-[#363636] flex items-center w-full no-underline py-2 px-4 font-normal hover:bg-nav-hover transition-colors duration-300"
+          className="text-nav-black flex items-center w-full no-underline py-2 px-4 font-normal hover:bg-nav-hover transition-colors duration-300"
         >
-          <MdOutlineDashboard className="text-[#898989] text-xl mx-2 w-6 h-6" />
+          <MdOutlineDashboard className="text-nav-grey text-xl mx-2" />
           <div>Dashboard</div>
         </Link>
         <Link
           href="/help"
           className="text-nav-black flex items-center w-full no-underline py-2 px-4 font-normal hover:bg-nav-hover transition-colors duration-300"
         >
-          <FiHelpCircle className="text-nav-grey text-xl mx-2 w-6 h-6" />
+          <FiHelpCircle className="text-nav-grey text-xl mx-2" />
           <div>Help</div>
         </Link>
       </div>
