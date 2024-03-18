852575654a2045a1fa3787e8443a4794b19daca9
Paulian Le <paulianle7@gmail.com>
2024-03-18 16:15:32 -0700

    added aqi color markers

--- src/components/AqiStatus.jsx
+++ src/components/AqiStatus.jsx
@@ -1,10 +1,7 @@
 import { FaCircle } from "react-icons/fa";
 import { statusOfAqi } from "@/data/StatusAqi";
 
-const AqiStatus = ({ value }) => {
-  // aqi calc for PM2.5 and PM10: concentration / standard x 100
-  // standard: 9 µg/m3 for PM2.5, and 150 µg/m3 for PM10
-
+const AqiStatus = ({ className, value }) => {
   const calcAqiColor = (value) => {
     const aqi = (value / 9) * 100;
 
@@ -24,8 +21,8 @@ const AqiStatus = ({ value }) => {
   const aqiColor = calcAqiColor(value);
 
   return (
-    <div className="flex items-center">
-      <FaCircle className={`text-base ${aqiColor} mr-2`} />
+    <div className={`flex items-center ${className}`}>
+      <FaCircle className={`${aqiColor} mr-2`} />
     </div>
   );
 };
--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -2,22 +2,22 @@
 import { Marker as LeafletMarker, Tooltip } from "react-leaflet";
 import AqiStatus from "@/components/AqiStatus";
 import L from "leaflet";
+import ReactDOMServer from "react-dom/server";
 
 const Marker = ({ sn, lat, lon, timestamp_local, measurements }) => {
-  const MARKER = `data:image/svg+xml;utf8,${encodeURIComponent(`<?xml version="1.0" encoding="iso-8859-1"?>
-  <svg width="58" height="57" viewBox="0 0 58 57" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M29.0254 27.3125C27.4507 27.3125 25.9404 26.6869 24.8269 25.5734C23.7134 24.4599 23.0879 22.9497 23.0879 21.375C23.0879 19.8003 23.7134 18.2901 24.8269 17.1766C25.9404 16.0631 27.4507 15.4375 29.0254 15.4375C30.6001 15.4375 32.1103 16.0631 33.2238 17.1766C34.3373 18.2901 34.9629 19.8003 34.9629 21.375C34.9629 22.1547 34.8093 22.9268 34.5109 23.6472C34.2125 24.3676 33.7752 25.0221 33.2238 25.5734C32.6725 26.1248 32.0179 26.5621 31.2976 26.8605C30.5772 27.1589 29.8051 27.3125 29.0254 27.3125ZM29.0254 4.75C24.6162 4.75 20.3875 6.50156 17.2697 9.61935C14.1519 12.7371 12.4004 16.9658 12.4004 21.375C12.4004 33.8438 29.0254 52.25 29.0254 52.25C29.0254 52.25 45.6504 33.8438 45.6504 21.375C45.6504 16.9658 43.8988 12.7371 40.781 9.61935C37.6632 6.50156 33.4346 4.75 29.0254 4.75Z" fill="#006DD1"/>
-  </svg>
-    `)}`;
+  const iconHtml = ReactDOMServer.renderToString(
+    <AqiStatus className="text-2xl" value={measurements.pm25} />,
+  );
 
-  const ICON = L.icon({
-    iconUrl: MARKER,
-    iconSize: [45, 45],
+  const ICON = L.divIcon({
+    html: iconHtml,
+    iconSize: [95, 100],
     iconAnchor: new L.Point(8, 8),
     popupAnchor: new L.Point(0, 0),
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
+    className: "border-none",
   });
 
   const handleClick = async () => {};
@@ -26,16 +26,16 @@ const Marker = ({ sn, lat, lon, timestamp_local, measurements }) => {
     lat &&
     lon && (
       <LeafletMarker
-        icon={ICON}
         position={[lat, lon]}
+        icon={ICON}
         eventHandlers={{
           click: handleClick,
         }}
       >
-        <Tooltip offset={[15, 0]} direction="top">
+        <Tooltip offset={[0, 0]} direction="top">
           <div className="flex flex-col px-2 space-y-3">
             <div className="flex flex-row items-center space-x-5 border-b border-tooltip-grey p-1">
-              <AqiStatus value={measurements.pm25} />
+              <AqiStatus className="text-xl" value={measurements.pm25} />
               <p className="font-normal text-base text-tooltip-black-100">
                 {sn}
               </p>
@@ -65,7 +65,7 @@ const Marker = ({ sn, lat, lon, timestamp_local, measurements }) => {
               </div>
 
               <div className="font-light text-base">
-                AQI: {((measurements.pm25 / 9) * 100).toFixed(2)}
+                AQI: {Math.round((measurements.pm25 / 9) * 100)}
               </div>
             </div>
           </div>
