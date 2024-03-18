0c0bbf8fed957bab864a2f2d1df6de06083f47cd
Paulian Le <paulianle7@gmail.com>
2024-03-18 15:14:50 -0700

    update legend stylign

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -6,23 +6,29 @@ const Legend = () => {
       <div className="text-sm font-normal mb-2">Air Quality Index</div>
 
       <div className="flex">
-        <div className="w-14 h-6 bg-aqi-green flex justify-center items-center text-xs">
-          0-50
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-14 h-4 bg-aqi-green flex justify-center items-center text-xs"></div>
+          <p className="text-xs">0-50</p>
         </div>
-        <div className="w-14 h-6 bg-aqi-yellow flex justify-center items-center text-xs">
-          51-100
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-14 h-4 bg-aqi-yellow flex justify-center items-center text-xs"></div>
+          <p className="text-xs">51-100</p>
         </div>
-        <div className="w-14 h-6 bg-aqi-orange flex justify-center items-center text-xs">
-          101-150
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-14 h-4 bg-aqi-orange flex justify-center items-center text-xs"></div>
+          <p className="text-xs">101-150</p>
         </div>
-        <div className="w-14 h-6 bg-aqi-red flex justify-center items-center text-xs">
-          151-200
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-14 h-4 bg-aqi-red flex justify-center items-center text-xs"></div>
+          <p className="text-xs">151-200</p>
         </div>
-        <div className="w-14 h-6 bg-aqi-purple flex justify-center items-center text-xs">
-          201-300
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-14 h-4 bg-aqi-purple flex justify-center items-center text-xs"></div>
+          <p className="text-xs">201-300</p>
         </div>
-        <div className="w-14 h-6 bg-aqi-maroon flex justify-center items-center text-xs">
-          300+
+        <div className="flex flex-col items-center gap-1">
+          <div className="w-14 h-4 bg-aqi-maroon flex justify-center items-center text-xs"></div>
+          <p className="text-xs">300+</p>
         </div>
       </div>
     </div>
