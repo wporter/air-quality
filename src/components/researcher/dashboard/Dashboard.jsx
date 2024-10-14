ba18a018287f8b9b519958765588bf8d8432928b
Sharan Singh <sharan.singh0203@gmail.com>
2024-10-14 14:03:22 -0700

    fixed the sensors

--- src/components/researcher/dashboard/Dashboard.jsx
+++ src/components/researcher/dashboard/Dashboard.jsx
@@ -7,25 +7,6 @@ import Data from "@/components/researcher/dashboard/Data";
 import { ArcGIS } from "@/components/Map/Map";
 
 const Dashboard = ({ locations, markers }) => {
-  const now = new Date().getTime();
-  // eslint-disable-next-line camelcase
-  const offlineSensors = markers.filter(({ timestamp_local }) => {
-    // eslint-disable-next-line camelcase
-    if (timestamp_local === "No data available") return true;
-    const sensorTime = new Date(timestamp_local).getTime();
-    const diffMinutes = (now - sensorTime) / (1000 * 60);
-    return diffMinutes > 60;
-  }).length;
-
-  // eslint-disable-next-line camelcase
-  const onlineSensors = markers.filter(({ timestamp_local }) => {
-    // eslint-disable-next-line camelcase
-    if (timestamp_local === "No data available") return false;
-    const sensorTime = new Date(timestamp_local).getTime();
-    const diffMinutes = (now - sensorTime) / (1000 * 60);
-    return diffMinutes <= 60;
-  }).length;
-
   return (
     <div className="h-screen w-full bg-air-white-100 pt-2">
       <div className="w-full h-1/2 bg-air-white-100 flex pl-8 pt-4">
@@ -35,14 +16,36 @@ const Dashboard = ({ locations, markers }) => {
             bg="bg-sensor-green"
             Icon={MdOutlineSignalWifi4Bar}
             color="text-green-400"
-            value={onlineSensors}
+            value={
+              markers.filter(
+                // eslint-disable-next-line camelcase
+                ({ timestamp_local }) =>
+                  parseInt(
+                    new Date(
+                      new Date().getTime() -
+                        new Date(timestamp_local).getTime(),
+                    ).getMinutes(),
+                  ) <= 60,
+              ).length
+            }
             text="Sensors Online"
           />
           <Status
             bg="bg-sensor-red"
             Icon={MdOutlineNetworkWifi1Bar}
             color="text-red-400"
-            value={offlineSensors}
+            value={
+              markers.filter(
+                // eslint-disable-next-line camelcase
+                ({ timestamp_local }) =>
+                  parseInt(
+                    new Date(
+                      new Date().getTime() -
+                        new Date(timestamp_local).getTime(),
+                    ).getMinutes(),
+                  ) > 60,
+              ).length
+            }
             text="Sensors Offline"
           />
         </div>
