b5b4383329931b4805dcd6e31dcb55e5bba5d935
Sharan Singh <sharan.singh0203@gmail.com>
2025-02-16 13:47:02 -0800

    indoor sensors removed

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,25 @@ import { ArcGIS } from "@/components/Map/Map";
 const Page = async () => {
   const { items } = await getMarkers();
 
+  const indoorSensors = [
+    "MOD-PM-00162",
+    "MOD-PM-00407",
+    "MOD-PM-00396",
+    "MOD-PM-00160",
+    "MOD-PM-00399",
+    "MOD-PM-00405",
+  ];
+
+  const outdoorMarkers = items.filter(
+    (item) => !indoorSensors.includes(item.sn),
+  );
+
   return (
     <div className="flex flex-col justify-center items-center relative">
       <Welcome />
       <div className="my-10 w-11/12" id="public-view-section">
         <p className="my-4 font-bold text-5xl">Public View</p>
-        <ArcGIS height="h-[60vh]" width="w-full" markers={items} />
+        <ArcGIS height="h-[60vh]" width="w-full" markers={outdoorMarkers} />
       </div>
 
       <Footer />
